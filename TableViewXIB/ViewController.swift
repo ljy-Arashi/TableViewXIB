06a4e71bf77c5a55b2af8f7b6eea366ff986bf69
刘佳艳 <644419778@qq.com>
2020-01-13 11:54:15 +0900

    Initial Commit

---
+++ TableViewXIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewXIB
+//
+//  Created by 刘佳艳 on 2020/01/13.
+//  Copyright © 2020 刘佳艳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
